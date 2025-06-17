3c07deec705887e58ea5da6d413ba1ebd5ec3555
yarden-fishler-dev <yarden.fishler@env0.com>
2025-06-17 15:43:43 +0300

    Update 6

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -3,3 +3,4 @@ resource "null_resource" "null2" {}
 resource "null_resource" "null3" {}
 resource "null_resource" "null4" {}
 resource "null_resource" "null5" {}
+resource "null_resource" "null6" {}
