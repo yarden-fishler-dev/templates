58ecea0b760213ea528c0ac518e142b0ee317037
Yarden Fishler <yarden.fishler@env0.com>
2025-08-03 17:45:10 +0300

    Added large plan

---
+++ misc/large-plan/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 0.13"
+}
+
+# Number of null resources to generate
+variable "resource_count" {
+  description = "How many null resources to create"
+  type        = number
+  default     = 1000
+}
+
+# Index list [0,1,2..]
+locals {
+  indices       = [for i in range(var.resource_count) : i]
+  # This value changes every plan/apply run and will therefore force the
+  # resources to be recreated on every re-deployment.
+  redeploy_time = timestamp()
+}
+
+resource "null_resource" "bulk" {
+  for_each = toset(local.indices)
+
+  triggers = {
+    index         = each.value
+    redeploy_time = local.redeploy_time
+  }
+}
