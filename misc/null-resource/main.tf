e3f139a7b0153bb29556badf01d00e98640fd602
Yarden Fishler <yarden.fishler@env0.com>
2025-12-02 16:38:03 +0200

    Added redundant variable

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -3,3 +3,8 @@ resource "null_resource" "null" {
 
 resource "null_resource" "null-resource-not-matching-conventions" {
 }
+
+variable "unused_variable" {
+  type    = string
+  default = "this variable is not used anywhere"
+}
